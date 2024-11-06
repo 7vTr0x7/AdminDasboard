dabd6f7cf07d0fe9aa668f04973f044848035d3f
7vTr0x7 <7vTrox7@gmail.com>
2024-11-06 13:03:23 +0530

    fix: reset password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,23 @@ import ForgotPassword from "./components/ForgotPassword";
 import Logout from "./components/Logout";
 import Home from "./pages/Home";
 import ResetPassword from "./components/ResetPassword";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+        </Routes>
+      </Router>
+      <Toaster />
+    </div>
   );
 }
 
--- frontend/src/components/ResetPassword.jsx
+++ frontend/src/components/ResetPassword.jsx
@@ -2,44 +2,43 @@ import React, { useState } from "react";
 import { auth } from "../firebaseConfig";
 import { confirmPasswordReset } from "firebase/auth";
 import { useSearchParams } from "react-router-dom";
+import { FaRegEyeSlash } from "react-icons/fa6";
+import { FaRegEye } from "react-icons/fa6";
+import toast from "react-hot-toast";
 
 function ResetPassword() {
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [message, setMessage] = useState(null);
-  const [isError, setIsError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [searchParams] = useSearchParams();
   const oobCode = searchParams.get("oobCode");
 
   const handlePasswordReset = async (e) => {
     e.preventDefault();
+
     if (newPassword !== confirmPassword) {
-      setMessage("Passwords do not match.");
-      setIsError(true);
+      toast.error("Passwords do not match.");
       return;
     }
 
     if (newPassword.length < 6) {
-      setMessage("Password should be at least 6 characters long.");
-      setIsError(true);
+      toast.error("Password should be at least 6 characters long.");
       return;
     }
 
     setLoading(true);
-    setMessage(null);
 
     try {
       await confirmPasswordReset(auth, oobCode, newPassword);
-      setMessage(
+      toast.success(
         "Password reset successfully. You can now log in with your new password."
       );
-      setIsError(false);
     } catch (error) {
-      setMessage(
+      toast.error(
         "Failed to reset password. Please try again or request a new link."
       );
-      setIsError(true);
       console.error(error.message);
     } finally {
       setLoading(false);
@@ -52,32 +51,40 @@ function ResetPassword() {
         <h1 className="text-2xl font-bold text-center text-gray-800">
           Reset Your Password
         </h1>
-        {message && (
-          <p
-            className={`text-center ${
-              isError ? "text-red-500" : "text-green-500"
-            }`}>
-            {message}
-          </p>
-        )}
 
         <form className="space-y-4" onSubmit={handlePasswordReset}>
-          <input
-            type="password"
-            placeholder="New Password"
-            className="input-field"
-            value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Confirm New Password"
-            className="input-field"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="New Password"
+              className="input-field w-full"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              required
+            />
+            <span
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+              onClick={() => setShowPassword(!showPassword)}>
+              {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+            </span>
+          </div>
+
+          <div className="relative">
+            <input
+              type={showConfirmPassword ? "text" : "password"}
+              placeholder="Confirm New Password"
+              className="input-field w-full"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+            <span
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+              onClick={() => setShowConfirmPassword(!showConfirmPassword)}>
+              {showConfirmPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+            </span>
+          </div>
+
           <button
             type="submit"
             className={`button w-full ${
